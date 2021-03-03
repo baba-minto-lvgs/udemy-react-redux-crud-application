371b216586fb5bf27d178cb126eef8a39c3974b4
baba-minto-lvgs <minto.baba@leverages.jp>
2021-03-03 22:02:30 +0900

    コンポーネント

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-      return (
-          <>
-              <label>label</label>
-              <input type="text" onChange={() => {alert("test")}}/>
-          </>
-          );
-  };
-}
+// class App extends Component {
+//   render() {
+//       return (
+//           <>
+//               <label>label</label>
+//               <input type="text" onChange={() => {alert("test")}}/>
+//           </>
+//           );
+//   };
+// }
+
+const App = () => {
+    return (
+        <div>
+            <Cat/>
+            <Cat/>
+            <Cat/>
+            <Cat/>
+        </div>
+
+
+        )
+
+};
+
+const Cat = () => {
+    return <div>Meow!</div>
+};
 
 export default App;
